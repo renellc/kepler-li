7ac2808a44a2a5c5889e73b3aa69cfadefb1c349
Renell Castro <rcskca@gmail.com>
2019-03-27 06:10:09 -0600

    Clicking toolbar title will take you to back to the home page

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,25 +1,27 @@
 <template>
   <v-toolbar>
-    <v-toolbar-title class="headline text-uppercase">
+    <v-toolbar-title 
+      id="title" 
+      class="headline text-uppercase" 
+      @mouseover="mouseOver()" 
+      @click="pushPage('/')"
+    >
       <span>Kepler</span>
       <span class="font-weight-light">li</span>
     </v-toolbar-title>
+
     <v-spacer></v-spacer>
+
     <v-toolbar-items>
       <template v-if="windowDim.width >= 425">
-        
-        <v-btn 
-          flat
-          to="/about"
-          >
+        <v-btn flat @click="pushPage('/about')">
           <v-icon>info</v-icon>&nbsp;About
         </v-btn>
-        <v-btn 
-          flat
-          >
+        <v-btn flat>
           <v-icon>link</v-icon>&nbsp;Links
         </v-btn>
       </template>
+
       <template v-else>
         <v-btn icon flat>
           <v-icon>info</v-icon>
@@ -28,11 +30,14 @@
           <v-icon>link</v-icon>
         </v-btn>
       </template>
+      
     </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
+import Router from "../router.js";
+
 export default {
   name: "Toolbar",
   data() {
@@ -40,7 +45,8 @@ export default {
       windowDim: {
         width: window.innerWidth,
         height: window.innerHeight
-      }
+      },
+      hovering: false
     };
   },
   mounted() {
@@ -52,6 +58,15 @@ export default {
         };
       });
     });
+  },
+  methods: {
+    pushPage: function(path) {
+      Router.push(path);
+    },
+    mouseOver: function() {
+      this.hovering = true;
+      document.getElementById('title').style.cursor = this.hovering ? "pointer" : "default";
+    }
   }
 };
 </script>
