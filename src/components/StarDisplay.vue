8f767f476c9e5acd9d6b9a0c04eb653089339c93
Renell Castro <rcskca@gmail.com>
2019-03-30 05:07:01 -0600

    Added StarDisplay component that receives a single object that contains a star's data

---
+++ src/components/StarDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    This is star {{ starData.starId }}.<br/>
+    Has exoplanets?: {{ starData.hasPossibleExoplanets }}<br/>
+    My range: {{ range }}<br/>
+    Standard deviation: {{ starData.stdDev }}<br/>
+    Points: {{ starData.simplifiedPoints }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StarDisplay',
+  props: {
+    starData: {
+      starid: Number,
+      haspossibleexoplanets: Boolean,
+      min: Number,
+      max: Number,
+      std: Number,
+      simplified: Array
+    },
+  },
+  mounted() {
+    console.log(this.$props);
+  },
+  computed: {
+    range: function() {
+      return this.starData.max - this.starData.min;
+    },
+  },
+}
+</script>
+
--- src/views/Stars.vue
+++ src/views/Stars.vue
@@ -1,20 +1,44 @@
 <template>
   <div>
     <h1>Stars</h1>
+    <StarDisplay
+      v-if="gotData"
+      v-bind:starData="starData[0]"
+    />
   </div>
 </template>
 
 <script>
+import StarDisplay from "../components/StarDisplay";
+
 export default {
   name: "Stars",
-  mounted() {
-    fetch("https://api-keplerli.herokuapp.com/api/stars")
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-      });
+  components: {
+    StarDisplay
+  },
+  data() {
+    return {
+      starData: null,
+      gotData: false,
+    };
+  },
+  created() {
+    this.getStarData();
+  },
+  watch: {
+    '$route': 'getStarData'
+  },
+  methods: {
+    getStarData: function() {
+      fetch("https://api-keplerli.herokuapp.com/api/stars")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.starData = data;
+          this.gotData = true;
+        });
+    }
   }
 };
 </script>
