75203b1001e99d146e0b4370208d2ae0137e0ca2
Renell Castro <rcskca@gmail.com>
2019-03-30 20:37:05 -0600

    Star page now fetches data based on the star id the page is given

--- src/views/Star.vue
+++ src/views/Star.vue
@@ -3,8 +3,31 @@
 </template>
 
 <script>
+import Router from '../router.js';
+
 export default {
-  name: "Star"
+  name: "Star",
+  mounted() {
+    this.fetchStar();
+  },
+  methods: {
+    fetchStar: function() {
+      fetch(`http://localhost:4000/api/stars/${this.$route.params.starId}`)
+        .then(res => {
+          if (res.status === 404) {
+            Router.push('/error');
+          } else {
+            return res.json();
+          }
+        })
+        .then(data => {
+          console.log(data);
+        })
+        .catch(fetchErr => {
+          console.log(fetchErr);
+        });
+    }
+  }
 };
 </script>
 
