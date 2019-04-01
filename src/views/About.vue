f50c50b2a36f17780157dc87814a67a5338f69f6
Renell Castro <rmc424@mail.usask.ca>
2019-04-01 17:50:26 -0600

    Added animation to Home page, updated styling on About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,31 +1,26 @@
 <template>
   <v-container>
-    <v-layout column justify-center>
-      <v-flex xs12 text-xs-center>
-        <h1><u>About</u></h1>
+    <v-layout column justify-space-around align-center fill-height>
+      <v-flex xs12 text-xs-center tag="h1">
+        About
       </v-flex>
 
-      <v-flex xs6 text-xs-center>
-        <p wrap>
-          KeplerLI is a small web application that allows you to view a given star's fluctuating
-          light intensity over a certain period of time. These fluctuations are particularly
-          interesting as they can tell us whether or not a star might have one or more exoplanets
-          orbiting around it. As celestial bodies pass in front of the star, we notice that the
-          light intensity received decreases. Recurring fluctuations in the received measured
-          light intensity might indicate that one or more exoplanets might be orbiting the star.
-          Though this is not conclusive evidence that a star has planets orbiting it, it can
-          reveal certain characteristics that match those of already discovered exoplanets, helping
-          us refine the way we search for exoplanets.
-        </p>
+      <v-flex xs6 text-xs-center wrap tag="p">
+        KeplerLI is a small web application that allows you to view a given star's fluctuating
+        light intensity over a certain period of time. These fluctuations are particularly
+        interesting as they can tell us whether or not a star might have one or more exoplanets
+        orbiting around it. As celestial bodies pass in front of the star, we notice that the
+        light intensity received decreases. Recurring fluctuations in the received measured
+        light intensity might indicate that one or more exoplanets might be orbiting the star.
+        Though this is not conclusive evidence that a star has planets orbiting it, it can
+        reveal certain characteristics that match those of already discovered exoplanets, helping
+        us refine the way we search for exoplanets.
       </v-flex>
-
-      <v-flex x6 text-xs-center>
-        <p wrap>
-          The data found in this dataset were derived from the observations made by the Kepler
-          Space Telescope and neatly organized by user WΔ on Kaggle. This web application only
-          displays a small portion of the full dataset that was organized by WΔ. The link to the
-          Kaggle page containing the full dataset can be found in the links page.
-        </p>
+      <v-flex xs6 text-xs-center wrap tag="p">
+        The data found in this dataset were derived from the observations made by the Kepler
+        Space Telescope and neatly organized by user WΔ on Kaggle. This web application only
+        displays a small portion of the full dataset that was organized by WΔ. The link to the
+        Kaggle page containing the full dataset can be found in the links page.
       </v-flex>
     </v-layout>
   </v-container>
@@ -36,3 +31,16 @@ export default {
   name: "About"
 };
 </script>
+
+<style scoped>
+h1 {
+  margin-bottom: 0.5em;
+  font-size: 350%;
+  font-style: normal;
+  font-weight: 300;
+}
+p {
+  line-height: 2.0em;
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <v-btn @click="pushPage('/stars')">Stars</v-btn>
-  </div>
+  <v-container fill-height text-xs-center align-content-center>
+    <v-layout row align-content-center wrap>
+      <v-flex xs12 wrap>
+        <h1>KeplerLI</h1>
+        <p>
+          Observe the fluctuating light intensity for hundreds of stars in the effort to find
+          exoplanets outside of our solar system.
+        </p>
+        <v-btn color="primary" class="home-button">Begin</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -17,3 +25,35 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+h1 {
+  font-size: 350%;
+  font-weight: 300;
+  margin-bottom: 0.5em;
+  animation: fadein 1.5s;
+}
+p {
+  opacity: 0;
+  line-height: 2em;
+  margin-bottom: 1.5em;
+  animation: fadein 1.5s forwards;
+  animation-delay: 0.5s;
+}
+
+.home-button {
+  opacity: 0;
+  animation: fadein 1.5s forwards;
+  animation-delay: 1.0s;
+}
+</style>
+
