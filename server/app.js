7b8707dc7a85c7bb50a0c2a422ec44671137ba4a
Renell Castro <rmc424@mail.usask.ca>
2019-04-07 16:44:02 -0600

    updated package.json

--- server/app.js
+++ server/app.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const history = require('connect-history-api-fallback');
+const serveStatic = require('serve-static');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 80;
 const distDir = path.join(__dirname, '../dist/');
 
 app.use(cors());
 app.use(history());
-app.use(express.static(distDir));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(distDir, 'index.html'));
-});
-
+app.use(serveStatic(distDir));
 app.listen(port);
