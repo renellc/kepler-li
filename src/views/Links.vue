728eeb5713351cfb8671cbadfc47592e914a7c2d
Renell Castro <rmc424@mail.usask.ca>
2019-04-07 09:04:26 -0600

    Added links page

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -2,13 +2,24 @@
   <v-container>
     <v-layout column justify-space-around align-center fill-height>
       <v-flex xs12 text-xs-center tag="h1">Links</v-flex>
+      <v-flex v-for="link in links" :key="link.title" xs12 text-xs-center wrap tag="p">
+        {{ link.title }}: <a :href="link.link">{{ link.link }}</a>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "Links"
+  name: "Links",
+  data() {
+    return {
+      links: [
+        { title: 'Dataset', link: 'https://www.kaggle.com/keplersmachines/kepler-labelled-time-series-data' },
+        { title: 'My GitHub', link: 'https://github.com/renellc' }
+      ]
+    }
+  }
 };
 </script>
 
@@ -31,5 +42,20 @@ h1 {
   font-style: normal;
   font-weight: 300;
   animation: slowmoveup 1.5s forwards;
+}
+
+p {
+  opacity: 0;
+  line-height: 2em;
+  transition-delay: 0.5s;
+  animation: slowmoveup 1.5s forwards;
+}
+
+p:nth-child(2) {
+  animation-delay: 0.5s;
+}
+
+p:nth-child(3) {
+  animation-delay: 0.85s;
 }
 </style>
