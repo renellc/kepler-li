59a86a3da83ece7c0de75c7bc8b9beac1a4b86cf
Renell Castro <rmc424@mail.usask.ca>
2019-04-07 09:59:10 -0600

    Used path package to combine directory names

--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const history = require('connect-history-api-fallback');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 4000;
+const distDir = path.join(__dirname, '../dist/');
 
 app.use(cors());
 app.use(history());
-app.use(express.static(__dirname + '/../dist/'));
+app.use(express.static(distDir));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/../dist/index.html');
+  res.sendFile(path.join(distDir, 'index.html'));
 });
 
 app.listen(port);
