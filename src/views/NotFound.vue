2369392984f24f22b232a61ee55b7d8597760924
Renell Castro <rmc424@mail.usask.ca>
2019-04-07 21:00:38 -0600

    Added proper content to Not Found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    Page requested not found.
-  </div>
+  <v-container>
+    <v-layout column justify-space-around align-center fill-height>
+      <v-flex xs12 text-xs-center tag="h1">Page Not Found</v-flex>
+      <v-flex xs6 text-xs-center wrap tag="p">
+        The page requested could not be accessed at this time.
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "NotFound",
-}
+  name: "NotFound"
+};
 </script>
+
+<style scoped>
+h1 {
+  opacity: 0;
+  margin-bottom: 0.5em;
+  font-size: 350%;
+  font-style: normal;
+  font-weight: 300;
+  animation: slowmoveup 1.5s forwards;
+}
+p {
+  opacity: 0;
+  line-height: 2em;
+  transition-delay: 0.5s;
+  animation: slowmoveup 1.5s forwards;
+}
+
+p:nth-child(2) {
+  animation-delay: 0.5s;
+}
+</style>
